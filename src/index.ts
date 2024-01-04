23e42a82e1a9f2a631e699e0963623d402a890ea
jacob <jmpecx@outlook.com>
2024-01-05 01:36:18 +0800

    chore:init

---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import * as bip39 from 'bip39'
+import {type Event, nip13} from 'nostr-tools'
+import {initNostrWasm} from 'nostr-wasm'
+import Web3 from 'web3'
+import Ws from 'ws'
+import winston from 'winston'
+import {sleep} from "./util.js";
+import {startWorkers} from "./worker.js";
+import 'dotenv/config'
+
+const myFormat = winston.format.printf((rr) => {
+    const {level, message, label, ms} = rr;
+    // console.log(rr)
+    return `[${ms} ${level}]: ${message}`;
+});
+
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.combine(
+        // winston.format.simple(),
+        winston.format.splat(),
+        winston.format.colorize(),
+        winston.format.ms(),
+        myFormat,
+    ),
+    // defaultMeta: {service: 'noss'},
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        // new winston.transports.File({ filename: 'combined.log' }),
+
+        new winston.transports.Console()
+    ],
+});
+
+async function main() {
+    // const a = 'affair short gallery tone much fish error anger clarify strike violin brown'
+    // //
+    // if (!bip39.validateMnemonic(a)) {
+    //     throw new Error('Key Error')
+    // }
+
+    logger.info('started !')
+    const {generateSecretKey, getPublicKey, verifyEvent, finalizeEvent} = await initNostrWasm();
+    let lastEventId = undefined;
+    const ws = new Ws('wss://report-worker-2.noscription.org/')
+    ws.onopen = () => {
+        // ws.send('Message From Client')
+        logger.info('ws connected')
+    }
+    ws.onerror = (error) => {
+        console.log(`WebSocket error: ${error.message}`)
+    }
+
+    ws.onmessage = (e) => {
+        const {eventId, seqWitness: s} = JSON.parse(e.data as string)
+        lastEventId = eventId;
+        // console.log('newest event id:' + lastEventId)
+    }
+    const privateKey = Buffer.from(process.env.PRIVATE_KEY as string, 'hex')
+    const publicKeyStr = process.env.PUBLIC_KEY as string;
+
+    // 获取最新的区块.
+    let startTime = Date.now()
+    let blockNum = BigInt(0);
+    const endpoints = ["https://rpc.ankr.com/arbitrum", "https://arbitrum.blockpi.network/v1/rpc/public", "https://arb1.arbitrum.io/rpc", "https://1rpc.io/arb", "https://arbitrum.llamarpc.com", "https://arb-pokt.nodies.app", "https://arbitrum-one.public.blastapi.io", "https://arb-mainnet-public.unifra.io", "https://arbitrum.api.onfinality.io/public", "https://arbitrum-one.publicnode.com", "https://arbitrum.meowrpc.com", "https://arbitrum.drpc.org"]
+    let tagSeqWitness: string[] = []
+    while (true) {
+        try {
+            if (!lastEventId) {
+                await sleep(500)
+                continue;
+            }
+            const web3 = new Web3(endpoints[Math.floor(Math.random() * endpoints.length)]);
+            if (blockNum === BigInt(0) || Date.now() - startTime > 1000 * 60) {
+                blockNum = await web3.eth.getBlockNumber();
+                startTime = Date.now();
+            }
+            const newBlockNumber = Number(blockNum) + Math.floor((Date.now() - startTime) / 1e3 * 4)
+            // 此后呢，我们自动累加区块，然后获取区块详情.
+            const block = await web3.eth.getBlock(newBlockNumber)
+            if (block) {
+                // const timepassed = BigInt(Math.floor(Date.now() / 1000)) - block.timestamp
+                // 成功找到区块.
+                tagSeqWitness = ["seq_witness", newBlockNumber.toString(), block.hash!]
+                // 更新见证者成功.
+            } else {
+                // 寻找区块失败.
+                logger.error('寻找区块失败.')
+                continue;
+            }
+
+            const event: any = {
+                // "sig": "f2fd270575f435d1192ce07973a92fd213bd35238d3750209dc83aab5355bfcc79832ef4f9aa42dc1ffac1599aa8cb19d540dabba5a66b2f6eda1287204d22e1",
+                // "id": "0000023d48d8dbaab0597c1459c1895718ade4bf77559ad0a596a1da214fa0e8",
+                "kind": 1,
+                // "created_at": 1704363094,
+                "tags": [["p", "9be107b0d7218c67b4954ee3e6bd9e4dba06ef937a93f684e42f730a0c3d053c"],
+                    ["e", "51ed7939a984edee863bfbb2e66fdc80436b000a8ddca442d83e6a2bf1636a95", "wss://relay.noscription.org/", "root"],
+                    ["e", lastEventId, "wss://relay.noscription.org/", "reply"],
+                    tagSeqWitness,
+                ],
+                // ["nonce", "0ypzk4h9uu3", "21"]],
+                "content": "{\"p\":\"nrc-20\",\"op\":\"mint\",\"tick\":\"noss\",\"amt\":\"10\"}",
+                "pubkey": publicKeyStr
+            }
+            logger.log('info', '获取区块完成,开始求解...hash:%s,block:%s', block.hash, block.number)
+            const timer1 = Date.now();
+            const eventWithNip13 = await startWorkers(
+                event,
+                21,
+                privateKey
+            ) as Event
+            logger.log('info', '计算hash完成，用时%s ms', Date.now() - timer1)
+            finalizeEvent(eventWithNip13, privateKey)
+            // console.log(eventWithNip13)
+            // verifyEvent(eventWithNip13)
+            // post
+            logger.info('求解成功...,准备提交')
+            const result = await fetch("https://api-worker.noscription.org/inscribe/postEvent", {
+                "headers": {
+                    "accept": "application/json, text/plain, */*",
+                    "accept-language": "zh-CN,zh;q=0.9",
+                    "content-type": "application/json",
+                    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "\"Windows\"",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-site",
+                    // "x-gorgon": "16327071665c8d11dbbf1d09537273c029c5d0e144d4c554bf49eb22dd2d0bfe",
+                    "Referer": "https://noscription.org/",
+                    "Referrer-Policy": "strict-origin-when-cross-origin"
+                },
+                "body": JSON.stringify(eventWithNip13),
+                "method": "POST"
+            });
+            console.log(await result.json())
+        } catch (e) {
+            logger.error('胜败乃兵家常事:' + e)
+        }
+    }
+    // 最终事件也是ok的.
+    // console.log(verifyEvent(finalEvent))
+}
+
+main()
---
+++ src/worker.ts
@@ -0,0 +1,83 @@
+import {parentPort, Worker, isMainThread, workerData} from "node:worker_threads";
+import {type Event, nip13} from 'nostr-tools'
+import {initNostrWasm} from 'nostr-wasm'
+import {fileURLToPath} from "node:url";
+
+export function getPow(hex: string): number {
+    let count = 0
+
+    for (let i = 0; i < hex.length; i++) {
+        const nibble = parseInt(hex[i], 16)
+        if (nibble === 0) {
+            count += 4
+        } else {
+            count += Math.clz32(nibble) - 28
+            break
+        }
+    }
+
+    return count
+}
+
+const cpuIntensiveFunction = async (event: Event, difficulty: number) => {
+    const nw = await initNostrWasm()
+
+    const tag = ['nonce', '', difficulty.toString()]
+
+    event.tags.push(tag)
+
+    while (true) {
+        const now = Math.floor(new Date().getTime() / 1000)
+
+        if (now !== event.created_at) {
+            event.created_at = now
+        }
+
+        tag[1] = Math.random().toString(36).substring(2, 15)
+        event.id = nw.hashEvent(event);
+
+        // console.log(event.id)
+        if (getPow(event.id) >= difficulty) {
+            break
+        }
+    }
+    parentPort?.postMessage(event)
+    return event
+    // 执行占用 CPU 的操作
+    // ...
+
+    // 返回结果
+};
+
+if (!isMainThread) {
+    await cpuIntensiveFunction(workerData[0], workerData[1])
+}
+
+export const startWorkers = (data: Event, difficulty: number, prk: Buffer) => new Promise((resolve, reject) => {
+    if (isMainThread) {
+        // 创建工作线程
+        const cpuCount = parseInt(process.env.CPU_COUNT as string)
+        const workers = new Array(cpuCount);
+        for (let i = 0; i < workers.length; i++) {
+            workers[i] = new Worker(fileURLToPath(import.meta.url), {
+                // 传递数据
+                workerData: [data, difficulty],
+                // type: 'module'
+            });
+        }
+
+        // 监听每个工作线程的完成
+        for (const worker of workers) {
+            worker.on('message', (result: any) => {
+                // 只要有一个线程得到结果，就终止所有的运行
+                // console.log('result', result)
+                if (result) {
+                    for (const worker of workers) {
+                        worker.terminate();
+                    }
+                    resolve(result);
+                }
+            });
+        }
+    }
+})
